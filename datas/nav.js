f61dd9b77b839b8778884377397ed5bc13b592af
nanbin <735552503@qq.com>
2020-04-04 21:27:06 +0800

    no message

---
+++ datas/nav.js
@@ -0,0 +1,116 @@
+var navs = [{
+	"title": "采购管理",
+	"icon": "fa-cubes",
+	"spread": false,
+	"globalTabIdIndex":100,
+	"children": [{
+		"title": "导入订单",
+		"icon": "&#xe641;",
+		"href": "button.html",
+		"globalTabIdIndex":2
+	}, {
+		"title": "预采购单",
+		"icon": "&#xe63c;",
+		"href": "form.html",
+		"globalTabIdIndex":3
+	}, {
+		"title": "查询采购单",
+		"icon": "&#xe63c;",
+		"href": "table.html",
+		"globalTabIdIndex":4
+	}, {
+		"title": "操作出库单",
+		"icon": "&#xe609;",
+		"href": "nav.html",
+		"globalTabIdIndex":5
+	}, {
+		"title": "查询调入单",
+		"icon": "&#xe62a;",
+		"href": "tab.html",
+		"globalTabIdIndex":6
+	}, {
+		"title": "新建采购单",
+		"icon": "&#xe60c;",
+		"href": "auxiliar.html",
+		"globalTabIdIndex":7
+	}, {
+		"title": "采购汇总表",
+		"icon": "&#xe60c;",
+		"href": "auxiliar.html",
+		"globalTabIdIndex":8
+	}, {
+		"title": "对账单",
+		"icon": "&#xe60c;",
+		"href": "auxiliar.html",
+		"globalTabIdIndex":9
+	}]
+}, {
+	"title": "客户管理",
+	"icon": "fa-cogs",
+	"spread": false,
+	"globalTabIdIndex":300,
+	"children": [{
+		"title": "意向客户",
+		"icon": "fa-table",
+		"href": "page/customer/customerList.html",
+		"globalTabIdIndex": 18
+	}, {
+		"title": "报价管理",
+		"icon": "fa-navicon",
+		"href": "navbar.html",
+		"globalTabIdIndex": 19
+	}, {
+		"title": "到店记录",
+		"icon": "fa-navicon",
+		"href": "page/customer/arrival.html",
+		"globalTabIdIndex": 1900
+	}, {
+		"title": "跟踪记录",
+		"icon": "fa-navicon",
+		"href": "page/customer/tailafter.html",
+		"globalTabIdIndex": 1901
+	}]
+}, {
+	"title": "产品管理",
+	"icon": "fa-cogs",
+	"spread": false,
+	"globalTabIdIndex":200,
+	"children": [{
+		"title": "产品查询",
+		"icon": "fa-table",
+		"href": "page/product/testTableNest.html",
+		"globalTabIdIndex": 11
+	}, {
+		"title": "创建产品",
+		"icon": "fa-navicon",
+		"href": "page/product/createProduct.html",
+		"globalTabIdIndex": 12
+	}, {
+		"title": "创建品类",
+		"icon": "fa-navicon",
+		"href": "page/product/category.html",
+		"globalTabIdIndex": 13
+	}]
+}, {
+	"title": "财务管理",
+	"icon": "fa-cogs",
+	"spread": false,
+	"globalTabIdIndex":300,
+	"children": [{
+		"title": "财务记账",
+		"icon": "fa-table",
+		"href": "page/finance/finance_list.html",
+		"globalTabIdIndex": 14
+	}, {
+		"title": "采购付款",
+		"icon": "fa-navicon",
+		"href": "navbar.html",
+		"globalTabIdIndex": 16
+	}]
+}, {
+		"title": "权限管理",
+		"icon": "fa-stop-circle",
+		"href": "page/authorization/authorization.html",
+		"spread": false,
+		"globalTabIdIndex": 17
+	}];
---
+++ js/common.js
@@ -0,0 +1,34 @@
+/** common.js By Beginner Emain:zheng_jinfan@126.com HomePage:http://www.zhengjinfan.cn */
+layui.define(['layer'], function(exports) {
+	"use strict";
+
+	var $ = layui.jquery,
+		layer = layui.layer;
+
+	var common = {
+		/**
+		 * 抛出一个异常错误信息
+		 * @param {String} msg
+		 */
+		throwError: function(msg) {
+			throw new Error(msg);
+			return;
+		},
+		/**
+		 * 弹出一个错误提示
+		 * @param {String} msg
+		 */
+		msgError: function(msg) {
+			layer.msg(msg, {
+				icon: 5
+			});
+			return;
+		},
+		//接口地址
+		address :"http://localhost:8080/new_product/",
+
+
+	};
+
+	exports('common', common);
+});
